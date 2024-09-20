97438b5d1d1d673ce9693580fae499f66cb41ff4
JarnoHendriksen <jarno.hendriksen@gmail.com>
2024-09-20 19:45:49 +0200

    Added AABB to analysis

--- histograms.py
+++ histograms.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import open3d as o3d
 
 data = pd.read_csv("mesh_analysis_cache.csv")
 
@@ -13,14 +15,27 @@ avg_faces = number_faces.sum() / number_faces.size
 print("Average vertices: " + str(avg_verts))
 print("Average faces: " + str(avg_faces))
 
+volumes = []
+toolarge = 0
+
+for i in range(len(classes)):
+    volume = abs(data['minx'][i] - data['maxx'][i]) * \
+        abs(data['miny'][i] - data['maxy'][i]) * \
+        abs(data['minz'][i] - data['maxz'][i])
+    if volume < 10:
+        volumes.append(volume)
+    else:
+        toolarge += 1
+
 number_verts.plot.hist(bins=100)
-
 plt.show()
-
 number_faces.plot.hist(bins=100)
-
 plt.show()
-
 classes.value_counts().plot.bar()
-
+plt.show()
+print(np.average(volumes))
+print(np.var(volumes))
+print(f"Too large: {toolarge}")
+bins = np.arange(min(volumes), max(volumes) + .1, .1)
+plt.hist(volumes, bins=bins)
 plt.show()
--- mesh_analyze.py
+++ mesh_analyze.py
@@ -15,7 +15,7 @@ def analyze_mesh(mesh, file_path, shape_class):
     vertices = np.asarray(mesh.vertices)
     triangles = np.asarray(mesh.triangles)
 
-    print(f"Analyzing mesh with {len(vertices)} vertices and {len(triangles)} triangles.")
+    #print(f"Analyzing mesh with {len(vertices)} vertices and {len(triangles)} triangles.")
 
     # 计算每个三角形的三条边长
     # Calculate the three side lengths of each triangle
@@ -30,6 +30,8 @@ def analyze_mesh(mesh, file_path, shape_class):
     # Calculate the variance of edge length as an approximate indicator of grid uniformity
     edge_var = np.var(edge_lengths)
 
+    aabb = mesh.get_axis_aligned_bounding_box()
+
     # 返回模型属性信息，包括模型路径、顶点数、三角形数和边长方差
     # Return mesh attribute information, including path, number of vertices, number of triangles, and variance of side length
     return {
@@ -37,7 +39,13 @@ def analyze_mesh(mesh, file_path, shape_class):
         'class': shape_class,
         'vertices': len(vertices),
         'triangles': len(triangles),
-        'edge_var': edge_var
+        'edge_var': edge_var,
+        'minx' : aabb.min_bound[0],
+        'miny' : aabb.min_bound[1],
+        'minz' : aabb.min_bound[2],
+        'maxx' : aabb.max_bound[0],
+        'maxy' : aabb.max_bound[1],
+        'maxz' : aabb.max_bound[2]
     }
 
 # 批量读取并分析文件夹及子文件夹中的所有模型
@@ -51,6 +59,8 @@ def analyze_mesh_in_folder(folder_path):
 
     for shape_class in class_names:
         root_path = os.path.join(folder_path, shape_class)
+
+        print(f"Analyzing shapes of class {shape_class}")
 
         # files = [glob.glob(f, root_dir=root_path) for f in ["*.obj", "*.ply", "*.stl"]]
 
@@ -67,26 +77,11 @@ def analyze_mesh_in_folder(folder_path):
                 # start_time = time.time()
                 mesh = o3d.io.read_triangle_mesh(file_path)
                 mesh.compute_vertex_normals()
-                mesh_info = analyze_mesh(mesh, file_path, shape_class)
+                mesh_info = analyze_mesh(mesh, file, shape_class)
                 mesh_data.append(mesh_info)
                 # print(f"Finished analyzing {file_path} in {time.time() - start_time:.2f} seconds.")
             except Exception as e:
                 print(f"Error loading {file_path}: {e}")
-
-    # for root, dirs, files in os.walk(folder_path):
-    #     for file in files:
-    #         if file.endswith('.obj') or file.endswith('.ply') or file.endswith('.stl'):
-    #             file_path = os.path.join(root, file)
-    #             try:
-    #                 # print(f"Loading mesh: {file_path}")
-    #                 # start_time = time.time()
-    #                 mesh = o3d.io.read_triangle_mesh(file_path)
-    #                 mesh.compute_vertex_normals()
-    #                 mesh_info = analyze_mesh(mesh, file_path)
-    #                 mesh_data.append(mesh_info)
-    #                 # print(f"Finished analyzing {file_path} in {time.time() - start_time:.2f} seconds.")
-    #             except Exception as e:
-    #                 print(f"Error loading {file_path}: {e}")
 
     # 将分析结果放入DataFrame中
     # Put the analysis results into the DataFrame
