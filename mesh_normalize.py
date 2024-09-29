59a43c95024053cbbfe2d378dfab0af89548805d
Ophelia Lee <lige_ophelia@163.com>
2024-09-29 02:03:53 +0200

    Update mesh_normalize.py

--- mesh_normalize.py
+++ mesh_normalize.py
@@ -1,5 +1,8 @@
 import open3d as o3d
 import numpy as np
+from pathlib import Path
+import shutil
+import os.path
 
 # 加载3D模型
 mesh_path = "D00921.obj"  # 替换为你的模型文件路径
@@ -38,7 +41,6 @@ def mesh_flipping(mesh):
         mesh.scale(-1, center=(0, 0, 0))
     if mass_center[2] < 0: # z
         mesh.scale(-1, center=(0, 0, 0))
-
     return mesh
 
 # 4. 大小归一化(Size Normalization)：将模型缩放到标准大小
@@ -49,13 +51,45 @@ def mesh_resize(mesh, target_size=1.0):
     mesh.scale(scaling_factor, center=(0, 0, 0))
     return mesh
 
-mesh = mesh_translate(mesh)
-mesh = mesh_pose_alignment(mesh)
-mesh = mesh_flipping(mesh)
-mesh = mesh_resize(mesh)
-o3d.io.write_triangle_mesh("./test.obj", mesh)
+def mesh_normalize(mesh_path, save_path):
+    mesh = o3d.io.read_triangle_mesh(mesh_path)
+    mesh = mesh_translate(mesh)
+    mesh = mesh_pose_alignment(mesh)
+    mesh = mesh_flipping(mesh)
+    mesh = mesh_resize(mesh)
+    o3d.io.write_triangle_mesh(save_path, mesh, write_vertex_normals=True)
 
-o3d.visualization.draw_geometries([mesh], width=800, height=600, mesh_show_wireframe=True)
+def normalize_database(input_dir, output_dir):
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+
+    # 如果输出目录不存在，则创建
+    # Create output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith((".obj")):
+                input_mesh_path = os.path.join(root, file)
+                output_mesh_path = os.path.join(output_dir, os.path.relpath(input_mesh_path, input_dir))
+
+                output_subdir = os.path.dirname(output_mesh_path)
+                if not os.path.exists(output_subdir):
+                    os.makedirs(output_subdir)
+
+                print(f"Processing file: {input_mesh_path}")
+                mesh_normalize(input_mesh_path, output_mesh_path)
+            else:
+                input_file_path = os.path.join(root, file)
+                output_file_path = os.path.join(output_dir, os.path.relpath(input_file_path, input_dir))
+                shutil.copy2(input_file_path, output_file_path)
+
+input_directory = "./resampled_ShapeDatabase_INFOMR-master"
+output_directory = "./normalized_ShapeDatabase_INFOMR-master"
+
+normalize_database(input_directory, output_directory)
+print("finished")
 
 
 
