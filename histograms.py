cccdbc254a2c11646e2357eb5b29ddd7619f6e66
JarnoHendriksen <jarno.hendriksen@gmail.com>
2024-10-07 00:40:17 +0200

    Added translation, rotation, scale and flip analysis

--- histograms.py
+++ histograms.py
@@ -18,7 +18,7 @@ def makeHist(title: str, xlabel: str, data: list[float | int]):
     plt.title(title)
     plt.xlabel(xlabel)
     filename = title.lower().replace(" ", "_")
-    plt.savefig(f"figures/{filename}_{version}.png")
+    plt.savefig(f"figures/{filename}_{version}.pdf")
     plt.show()
 
 def analyze_sample_count():
@@ -44,10 +44,55 @@ def analyze_edge_variance():
     makeHist("Edge Length Variance", "Variances", edge_var_trunc)
 
 def analyze_translation():
-    print("Analyzing translation")
+    print("== Analyzing translation ==")
+    paths = cache_data['file']
+    distances = []
+
+    for p in paths:
+        mesh = o3d.io.read_triangle_mesh(p)
+        centroid = mesh.get_center()
+        dist = np.linalg.norm(centroid - np.array((0,0,0)))
+        if dist < 20:
+            distances.append(dist)
+        else:
+            print(dist)
+    
+    printStats("Distance to World Origin", distances)
+    makeHist("Distance to World Origin", "Distances", distances)
 
 def analyze_rotation():
-    print("Analyzing rotation")
+    print("== Analyzing rotation ==")
+    paths = cache_data['file']
+    dotxs = []
+    dotys = []
+    dotzs = []
+
+    invalid = 0
+
+    for p in paths:
+        mesh = o3d.io.read_triangle_mesh(p)
+        covariance = np.cov(np.array(mesh.vertices).T)
+        
+        try:
+            _, eigenvectors = np.linalg.eigh(covariance)
+            l1 = eigenvectors[:, 0] # x
+            l2 = eigenvectors[:, 1] # y
+            l3 = eigenvectors[:, 2] # z
+
+            dotxs.append(np.dot(l1, np.array((1, 0, 0))))
+            dotys.append(np.dot(l2, np.array((0, 1, 0))))
+            dotzs.append(np.dot(l3, np.array((0, 0, 1))))
+        except:
+            invalid += 1
+    
+    printStats("Dot l1 x", dotxs)
+    printStats("Dot l2 y", dotys)
+    printStats("Dot l3 z", dotzs)
+    print("\t Invalid: " + str(invalid))
+
+    makeHist("Dot Product L1", "L1 . x", dotxs)
+    makeHist("Dot Product L2", "L2 . y", dotys)
+    makeHist("Dot Product L3", "L3 . z", dotzs)
 
 def analyze_scale():
     print("== Analyzing scale ==")
@@ -70,7 +115,46 @@ def analyze_scale():
 
 
 def analyze_flip_direction():
-    print("Analyzing flip direction")
+    print("== Analyzing flip direction ==")
+    paths = cache_data['file']
+    xspos = xsneg = 0
+    yspos = ysneg = 0
+    zspos = zsneg = 0
+
+    invalid = 0
+
+    for p in paths:
+        mesh = o3d.io.read_triangle_mesh(p)
+        vertices = np.asarray(mesh.vertices)
+        mass_center = np.mean(vertices, axis=0)
+
+        if mass_center[0] < 0:  # x
+            xsneg += 1
+        elif mass_center[0] >= 0:
+            xspos += 1
+        if mass_center[1] < 0:  # x
+            ysneg += 1
+        elif mass_center[1] >= 0:
+            yspos += 1
+        if mass_center[2] < 0:  # x
+            zsneg += 1
+        elif mass_center[2] >= 0:
+            zspos += 1
+
+    fig, ax = plt.subplots(layout='constrained')
+    rects = ax.bar(np.array((-1, 1)), np.array((xsneg, xspos)), 0.25, label="x")
+    ax.bar_label(rects, padding=3)
+    rects = ax.bar(np.array((-1, 1)) + 0.25, np.array((ysneg, yspos)), 0.25, label="y")
+    ax.bar_label(rects, padding=3)
+    rects = ax.bar(np.array((-1, 1)) + 0.5, np.array((zsneg, zspos)), 0.25, label="z")
+    ax.bar_label(rects, padding=3)
+    ax.set_xticks(np.array((-1, 1)) + 0.25, np.array((-1, 1)))
+    ax.legend(loc='upper left', ncols=3)
+    ax.set_ylabel("Frequency")
+    ax.set_xlabel("Direction")
+    ax.set_title("Direction of Mass")
+    plt.savefig(f"figures/direction_of_mass_{version}.png")
+    plt.show()
 
 def main(values: list[str]):
     all_values = ["sample_count", "edge_variance", "translation", "rotation", "scale", "flip"]
