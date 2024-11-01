88ac45e29aa62f68026938cc36564ace906a533f
Ophelia Lee <lige_ophelia@163.com>
2024-11-01 21:20:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ normalize_database("ShapeDatabase_Resampled", "ShapeDatabase_Normalized")
 from mesh_descriptors import calculate_descriptor_for_the_database
 calculate_descriptor_for_the_database("ShapeDatabase_Normalized", 150000, 100)
 
-# Extract 3D property descriptors histogram datas and plot from the CSV file
-from visualize_3d_histogram import plot_histograms_from_csv
+# Extract shape property descriptors histogram datas and plot from the CSV file
+from visualize_shape_histogram import plot_histograms_from_csv
 plot_histograms_from_csv('descriptors.csv', 'D00138.obj')
 
 # Normalizing the histograms of the shape descriptors
