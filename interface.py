be854f41fc215fcf3ef70210910915167d3b09dc
Ophelia Lee <lige_ophelia@163.com>
2024-11-02 20:08:35 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+from sklearn.metrics.pairwise import euclidean_distances
 import streamlit as st
 import open3d as o3d
 import plotly.graph_objects as go
@@ -8,7 +9,7 @@ import trimesh
 import pymeshlab as pml
 import pandas as pd
 
-from mesh_querying import mesh_querying, mesh_querying_global
+from mesh_querying import mesh_querying, mesh_querying_global, process_new_model
 
 
 def load_model(file_path):
@@ -45,7 +46,7 @@ def show_3d_model(mesh, width=400):
     st.plotly_chart(fig, key=f'model_plot_{np.random.randint(0, 1000000)}')
 
 
-def match_model(input_mesh_path, csv_path):
+def match_model(input_mesh_path, csv_path, stats_path):
     st.write("Processing uploaded model...")
 
     query_mesh = load_model(input_mesh_path)
@@ -56,13 +57,12 @@ def match_model(input_mesh_path, csv_path):
     model_file_name = os.path.splitext(uploaded_file.name)[0] + '.obj'
 
     # Find similar models using the find_similar_models function
-    similar_models = mesh_querying(model_file_name, csv_path)
-    result = mesh_querying(model_file_name, csv_path)
+    class_name, result = mesh_querying(model_file_name, csv_path, stats_path)
     if isinstance(result, str):
         st.write(result)
         return
 
-    class_name, similar_models = result
+    # class_name, similar_models = result
     st.subheader("Top Similar Models")
 
     # Display similar models in a grid
@@ -71,7 +71,7 @@ def match_model(input_mesh_path, csv_path):
     # Create columns for display
     cols = st.columns(num_columns, gap='small')
 
-    for i, (file_name, distance) in enumerate(similar_models):
+    for i, ((class_name, file_name), distance) in enumerate(result):
         # Construct the file path based on class name and file name (adjust to your directory structure)
         model_file_path = os.path.join("ShapeDatabase", class_name, file_name)
 
@@ -79,6 +79,7 @@ def match_model(input_mesh_path, csv_path):
         similar_mesh = load_model(model_file_path)
 
         with cols[i % num_columns]:
+            st.write(f"Class Name: {class_name}")
             st.write(f"File Name: {file_name}")
             st.write(f"Distance = {distance:.4f}")
             show_3d_model(similar_mesh, width=600)
@@ -138,7 +139,7 @@ if uploaded_file is not None:
 
     # match
     if st.sidebar.button("Match Model"):
-        match_model(input_mesh_path, "descriptors_standardized.csv")
+        match_model(input_mesh_path, "descriptors_standardized.csv", "standardization_stats.csv")
 
 else:
     st.write("Matching Results will be displayed here after uploading a model.")
