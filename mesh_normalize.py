e170f1b5f8a383d0a60a472492a0d1219e9428f3
Ophelia Lee <lige_ophelia@163.com>
2024-11-15 21:53:25 +0100

    Update mesh_normalize.py

--- mesh_normalize.py
+++ mesh_normalize.py
@@ -24,7 +24,6 @@ def Rz(theta):
                    [0, 0, 1]])
 
 
-# 1. 平移：将模型的重心移动到原点
 # 1. Translation: Move the center of gravity of the model to the origin
 def mesh_translate(mesh):
     barycenter = mesh.get_center()
@@ -32,7 +31,6 @@ def mesh_translate(mesh):
     return mesh
 
 
-# 2. 姿态对齐：将模型的主要方向对齐到坐标系轴
 # 2. Pose Alignment: Align the main orientation of the model to the axis of the coordinate system
 def mesh_pose_alignment(mesh):
     covariance = np.cov(np.asarray(mesh.vertices).T)
@@ -76,7 +74,6 @@ def sign(n):
         return 1
 
 
-# 3. 翻转：使主要质量集中在正半轴
 # 3. Flipping: Concentrate the main mass on the positive half axis
 def mesh_flipping(mesh):
     # Calculate the mass distribution of the model on each axis
@@ -99,7 +96,6 @@ def mesh_flipping(mesh):
     return mesh
 
 
-# 4. 大小归一化：将模型缩放到标准大小
 # 4. Size Normalization: Scaling the model to a standard size
 def mesh_resize(mesh, target_size=1.0):
     bounding_box = mesh.get_axis_aligned_bounding_box()
@@ -109,6 +105,7 @@ def mesh_resize(mesh, target_size=1.0):
     return mesh
 
 
+# Do the normalize for a single model
 def mesh_normalize(mesh_path, save_path):
     mesh = o3d.io.read_triangle_mesh(mesh_path)
     mesh = mesh_translate(mesh)
@@ -118,6 +115,7 @@ def mesh_normalize(mesh_path, save_path):
     o3d.io.write_triangle_mesh(save_path, mesh, write_vertex_normals=True)
 
 
+# Do the normalize for the input model. Only uses for the query part
 def mesh_normalize_for_new(mesh):
     mesh = mesh_translate(mesh)
     mesh = mesh_pose_alignment(mesh)
@@ -130,7 +128,6 @@ def normalize_database(input_folder, output_folder):
     input_dir = Path(input_folder)
     output_dir = Path(output_folder)
 
-    # 如果输出目录不存在，则创建
     # Create output directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -151,22 +148,9 @@ def normalize_database(input_folder, output_folder):
                     os.makedirs(output_subdir)
 
                 mesh_normalize(input_mesh_path, output_mesh_path)
-
-                # try:
-                #     mesh_normalize(input_mesh_path, output_mesh_path)
-                # except:
-                #     failed += 1
             else:
                 input_file_path = os.path.join(root, file)
                 output_file_path = os.path.join(output_dir, os.path.relpath(input_file_path, input_dir))
                 shutil.copy2(input_file_path, output_file_path)
     
     print(f"Finished normalization ({failed} failed shapes)")
-
-
-#if __name__ == '__main__':
-#    input_directory = "./resampled_ShapeDatabase_INFOMR-master"
-#    output_directory = "./normalized_ShapeDatabase_INFOMR-master"
-
-#    normalize_database(input_directory, output_directory)
-#    print("finished")
