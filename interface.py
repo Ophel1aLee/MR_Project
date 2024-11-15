158e9f96653f515628e7f55abbf678280cf208dc
Ophelia Lee <lige_ophelia@163.com>
2024-11-15 22:41:02 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,16 +1,12 @@
-from sklearn.metrics.pairwise import euclidean_distances
 import streamlit as st
 import open3d as o3d
 import plotly.graph_objects as go
 import numpy as np
 import tempfile
 import os
-import trimesh
-import pymeshlab as pml
 import pandas as pd
 from pynndescent import NNDescent
-
-from mesh_querying import mesh_querying, mesh_querying_global, process_new_model, fast_query
+from mesh_querying import mesh_querying, fast_query
 
 
 def load_model(file_path):
@@ -79,46 +75,6 @@ def match_model(input_mesh_path, csv_path, stats_path, K=4, fastMatch = False, a
         # Construct the file path based on class name and file name (adjust to your directory structure)
         model_file_path = os.path.join(
             "ShapeDatabase_Normalized", class_name, file_name)
-
-        # Load and display the similar model
-        similar_mesh = load_model(model_file_path)
-
-        with cols[i % num_columns]:
-            st.write(f"Class Name: {class_name}")
-            st.write(f"File Name: {file_name}")
-            st.write(f"Distance = {distance:.4f}")
-            show_3d_model(similar_mesh, width=600)
-
-
-def match_model_global(input_mesh_path, csv_path):
-    st.write("Processing uploaded model...")
-
-    query_mesh = load_model(input_mesh_path)
-    st.subheader("Query Model")
-    show_3d_model(query_mesh, width=400)
-
-    # Extract the file name from the input path
-    model_file_name = os.path.splitext(uploaded_file.name)[0] + '.obj'
-
-    # Find similar models using the mesh_querying_global function
-    result = mesh_querying_global(model_file_name, csv_path)
-    if isinstance(result, str):
-        st.write(result)
-        return
-
-    similar_models = result
-
-    st.subheader("Top Similar Models Globally")
-
-    # Display similar models in a grid
-    # Set the number of models to display per row
-    num_columns = 2
-    # Create columns for display
-    cols = st.columns(num_columns, gap='small')
-
-    for i, ((class_name, file_name), distance) in enumerate(similar_models):
-        # Construct the file path based on class name and file name (adjust to your directory structure)
-        model_file_path = os.path.join('ShapeDatabase_Normalized', class_name, file_name)
 
         # Load and display the similar model
         similar_mesh = load_model(model_file_path)
